d60a79dfa535d1a986068432a6c9ec37fcbd59c6
Félix Orain <felix.orain@gmail.com>
2025-04-02 12:56:42 +0200

    Added a 404html file to try to redirect the browser to the good page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <StrictMode>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </StrictMode>
+  </BrowserRouter>
 );
